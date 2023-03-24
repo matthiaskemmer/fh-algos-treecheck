dfb23f7f606fc6eb12aaf42def5ac93323aecd87
Matthias Kemmer <mattkem@outlook.de>
2023-03-24 16:27:38 +0100

    Add basic project structure

---
+++ tree.py
@@ -0,0 +1,35 @@
+class Tree:
+    """Class representing an AVL tree."""
+    
+    def __init__(self):
+        self.root = None  # root node
+        self.stats = None  # tree statistics
+
+    def add_node(self, key: int):
+        pass
+
+    def search(self, value: int):
+        """Search whole tree."""
+        pass
+
+    def search_subtree(self):
+        """Search subtree."""
+        pass
+
+    def treecheck(self):
+        """Recursiv tree check."""
+        pass
+
+    def load_file(self, file_path: str):
+        """Load tree from text file."""
+        pass
+
+    def get_balance_factor(self):
+        pass
+
+    def update_tree_stats(self):
+        """Update tree statistics."""
+        # key min value
+        # key max value
+        # key arithmetic average
+        pass
