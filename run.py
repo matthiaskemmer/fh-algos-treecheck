73dbecd7c0343176ce802fe2db9d56ae05118242
Li <122217894+LWW1206@users.noreply.github.com>
2023-04-03 15:27:14 +0200

    Delete rotation & add AVL check
    * deleted rotation, avl check, stats
    
    * Update tree.py
    
    ---------
    
    Co-authored-by: Matthias Kemmer <126191759+matthiaskemmer@users.noreply.github.com>

--- run.py
+++ run.py
@@ -4,4 +4,6 @@ if __name__ == "__main__":
     file_path = input("Enter the path to the tree file: ")
     tree = Tree()
     tree.load_file(file_path)
-    tree.print_tree_recursive(tree.root)
+    #tree.print_tree_recursive(tree.root)
+    tree.treecheck(tree.root)
+    tree.print_tree_stats()
--- tree.py
+++ tree.py
@@ -6,6 +6,7 @@ class Tree:
     def __init__(self):
         self.root = None  # root node
         self.stats = None  # tree statistics
+        self.is_avl = 'yes' # base case : yes ; if violation -> no
 
     def add_node(self, key: int):
         self.root = self._add_node_recursive(self.root, key)
@@ -22,28 +23,11 @@ class Tree:
             # Key already exists in tree, do not insert again
             return curr_node
 
-        # Height of current node
+        # Update the height of the current node
         curr_node.height = 1 + max(self.get_height(curr_node.left), self.get_height(curr_node.right))
-
-        # get balance factor
-        balance = self.get_balance_factor(curr_node.left, curr_node.right)
-
-        if balance > 1 and key < curr_node.left.key:
-            return self.rotate_right(curr_node)
-
-        if balance < -1 and key > curr_node.right.key:
-            return self.rotate_left(curr_node)
-
-        if balance > 1 and key > curr_node.left.key:
-            curr_node.left = self.rotate_left(curr_node.left)
-            return self.rotate_right(curr_node)
-
-        if balance < -1 and key < curr_node.right.key:
-            curr_node.right = self.rotate_right(curr_node.right)
-            return self.rotate_left(curr_node)
-
-        return curr_node
     
+        return curr_node
+
     def get_height(self, node):
         """Get the height of a node in the tree."""
         if not node:
@@ -53,35 +37,8 @@ class Tree:
     def get_balance_factor(self, node_left, node_right):
         height_left = self.get_height(node_left)
         height_right = self.get_height(node_right)
-        balance_factor = height_left - height_right
+        balance_factor = height_right - height_left
         return balance_factor
-
-    
-    def rotate_left(self, node):
-        """Rotate the given node and its right child to the left."""
-        right_child = node.right
-        left_grandchild = right_child.left
-
-        right_child.left = node
-        node.right = left_grandchild
-
-        node.height = 1 + max(self.get_height(node.left), self.get_height(node.right))
-        right_child.height = 1 + max(self.get_height(right_child.left), self.get_height(right_child.right))
-
-        return right_child
-
-    def rotate_right(self, node):
-        """Rotate the given node and its left child to the right."""
-        left_child = node.left
-        right_grandchild = left_child.right
-
-        left_child.right = node
-        node.left = right_grandchild
-
-        node.height = 1 + max(self.get_height(node.left), self.get_height(node.right))
-        left_child.height = 1 + max(self.get_height(left_child.left), self.get_height(left_child.right))
-
-        return left_child
 
     def search(self, value: int):
         """Search whole tree."""
@@ -91,9 +48,21 @@ class Tree:
         """Search subtree."""
         pass
 
-    def treecheck(self):
-        """Recursiv tree check."""
-        pass
+    def treecheck(self, node):
+        if not node:
+            # An empty node is always an AVL node
+            return True
+
+        right_subtree_avl = self.treecheck(node.right)
+        left_subtree_avl = self.treecheck(node.left)
+
+        # Check if the node's height and balance factor are correct
+        height_diff = self.get_height(node.right) - self.get_height(node.left)
+        if abs(height_diff) > 1:
+            print(f"bal({node.key}) = {height_diff} (AVL violation!)")
+            self.is_avl = 'no'
+        else:
+            print(f"bal({node.key}) = {height_diff}")
 
     def load_file(self, file_path: str):
         """Load tree from text file."""
@@ -102,12 +71,40 @@ class Tree:
                 key = int(line.strip())
                 self.add_node(key)
 
-    def update_tree_stats(self):
+    def update_tree_stats(self, node):
         """Update tree statistics."""
-        # key min value
-        # key max value
-        # key arithmetic average
-        pass
+        if not node:
+            # An empty node is always an AVL node
+            return {'min': None, 'max': None, 'sum': 0, 'count': 0}
+        
+        self.update_tree_stats(node.right)
+        self.update_tree_stats(node.left)
+
+        # Calculate the min and max values
+        min_val = node.key
+        max_val = node.key
+        if left_stats['min'] is not None:
+            min_val = min(left_stats['min'], min_val)
+        if right_stats['min'] is not None:
+            min_val = min(right_stats['min'], min_val)
+        if left_stats['max'] is not None:
+            max_val = max(left_stats['max'], max_val)
+        if right_stats['max'] is not None:
+            max_val = max(right_stats['max'], max_val)
+        
+        # Calculate the sum and count of all key values
+        sum_vals = node.key + left_stats['sum'] + right_stats['sum']
+        count = 1 + left_stats['count'] + right_stats['count']
+
+        # Return a dictionary with the min, max, sum, and count
+        return {'min': min_val, 'max': max_val, 'sum': sum_vals, 'count': count}
+
+    def print_tree_stats(self):
+        """Print out tree statistics."""
+        stats = self.update_tree_stats(self.root)
+        
+        print(f"AVL:", (self.is_avl))
+        print(f"min: {stats['min']}, max: {stats['max']}, avg: {stats['sum'] / stats['count']}")
     
     def print_tree_recursive(self, current_node):
         if current_node: #equal to if current_node is None
